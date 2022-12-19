66e3d9ebfc353116cf2542c1161a87e0bfd51044
ibt-qad <90773021+haboosh77@users.noreply.github.com>
2022-12-19 16:23:03 +0300

    full app

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pw_validator/flutter_pw_validator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController controller = TextEditingController();
+  bool success = false;
+
+  
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: success ? Colors.green : Colors.red,
+          title: Column(
+            children: const [
+              Text("Password Validate"),
+
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 12.5),
+          child: SingleChildScrollView(
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Icon(
+                    success ? Icons.lock_open : Icons.lock_outline,
+                    color: success ? Colors.green : Colors.red,
+                    size: 100,
+                  ),
+                ),
+                SizedBox(
+                  height: 250,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: TextField(
+                            controller: controller,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Color.fromARGB(255, 207, 207, 207),
+                                        width: 2),
+                                    borderRadius: BorderRadius.circular(5)),
+                                floatingLabelStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: success ? Colors.green : Colors.red,
+                                      width: 2),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                hintText: "Password",
+                                border: const OutlineInputBorder(
+                                    borderSide: BorderSide()))),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      FlutterPwValidator(
+                        controller: controller,
+                        minLength: 8,
+                        uppercaseCharCount: 2,
+                        numericCharCount: 3,
+                        specialCharCount: 1,
+                        normalCharCount: 3,
+                        width: 400,
+                        height: 150,
+                        onSuccess: () {
+                          setState(() {
+                            success = true;
+                          });
+                          print("MATCHED ");
+                          print('wrote by Ibtehal Al-Qadry');
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text("Password is matched")));
+                        },
+                        onFail: () {
+                          setState(() {
+                            success = false;
+                          });
+                          print("NOT MATCHED");
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
